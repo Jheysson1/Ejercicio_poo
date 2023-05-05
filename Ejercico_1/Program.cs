50386c05e2d4f481072f029863dc461543243149
Jheysson1 <jheyssonmendoza4@gmail.com>
2023-05-04 20:53:58 -0600

    Ejercicio_1

---
+++ Ejercico_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercico_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo vehiculo1 = new Vehiculo(1, "Chevrolet", "Aveno", 5000, 1000, 2017);
+            Console.WriteLine(vehiculo1.ToString());
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+    }
+}
