88011a68e19e8eca9babd0b039b625d80b7c927e
Jheysson1 <jheyssonmendoza4@gmail.com>
2023-05-04 22:05:11 -0600

    Ejercicio_7

---
+++ Ejercicio_7/Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    class Estudiante : Persona
+    {
+        public Estudiante(string nombre) : base(nombre)
+        {
+
+        }
+        public void Estudiar()
+        {
+            Console.WriteLine("{0} está estudiando", Nombre);
+        }
+    }
+}
---
+++ Ejercicio_7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona[] personas = new Persona[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("De su nombre: ");
+                string nombre = Console.ReadLine();
+
+                if (i < 1)
+                {
+                    personas[i] = new Profesor(nombre);
+
+                }
+                else
+                {
+                    personas[i] = new Estudiante(nombre);
+
+                }
+            }
+
+            foreach (Persona persona in personas)
+            {
+                if (persona is Profesor)
+                {
+                    ((Profesor)persona).Explicar();
+                }
+                else
+                {
+                    ((Estudiante)persona).Estudiar();
+                }
+
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
