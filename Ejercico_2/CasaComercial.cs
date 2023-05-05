e010cf1d8f963aca1ad1712dd37c66980096934b
Jheysson1 <jheyssonmendoza4@gmail.com>
2023-05-04 21:04:42 -0600

    Ejercicio_2

---
+++ Ejercico_2/CasaComercial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercico_2
+{
+    class CasaComercial
+    {
+        //campo de la clase Casa Comercial
+        string nombre;
+        string correo;
+        int total;
+        //esto es composicion, ya que la clase casa comercial tiene una lista de objetos de de tipo vehiculo
+        //si hay una lista, se hace una composición
+        List<Vehiculo> vehiculos;
+
+        //Constructor
+        public CasaComercial(string nombre, string correo, int total, List<Vehiculo> vehiculos)
+        {
+            this.nombre = nombre;
+            this.correo = correo;
+            this.total = total;
+            this.vehiculos = vehiculos;
+        }
+        //Propiedades
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public int Total { get => total; set => total = value; }
+        internal List<Vehiculo> Vehiculos { get => vehiculos; set => vehiculos = value; }
+
+        //áñadimos cada uno de los metodos
+
+        public void aniadirVehiculo(Vehiculo V)
+        {
+            try
+            {
+                //añadimos un nuevo objeto a la lista vehiculo
+                this.vehiculos.Add(V);
+                //actualizamos el total de vehiculos
+                this.total += 1;
+                Console.WriteLine("Se añadió un nuevo vehiculo");
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se añadió un nuevo vehiculo debido a: \n" + ex.Message);
+                Console.WriteLine();
+
+            }
+
+        }
+
+        public void mostrarVehiculos()
+        {
+            foreach (Vehiculo v in this.vehiculos)
+            {
+                Console.WriteLine("------------");
+                Console.WriteLine(v.ToString());
+            }
+
+        }
+
+        public void vaciarVehiculo()
+        {
+            Console.WriteLine("Está segguro desea eliminar todos los elementos mostrados en la lista?: S/N");
+            string response = Console.ReadLine();
+            if (response == "S" || response == "s")
+            {
+                vehiculos = new List<Vehiculo>();
+                this.total = 0;
+                Console.WriteLine("Se han eliminado los objetos correctamente");
+
+            }
+            else
+            {
+                Console.WriteLine("No se eliminó, Hay {0} vehiculos registrados", total);
+                Console.WriteLine("Enter para continuar.");
+            }
+            Console.ReadLine();
+        }
+
+        public void eliminarVehiculo()
+        {
+            vehiculos.Clear();
+            total = 0;
+            Console.WriteLine("Se ha eliminado el vehiculo", total);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Ejercico_2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercico_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CasaComercial cc2 = new CasaComercial("Casa Pelas", "contacto@casapellas.com", 0, new List<Vehiculo>());
+            cc2.aniadirVehiculo(new Vehiculo(2, "Kia", "Prado", 3999, 4999, 2004));
+
+
+
+            Console.WriteLine("De el nombre de la casa comercial: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el email: ");
+            string contacto = Console.ReadLine();
+            //definimos un nuevo objeto de tipo Casa Comercial
+            CasaComercial cc = new CasaComercial(nombre, contacto, 0, new List<Vehiculo>());
+
+            //informacion del vehiculo a añadir
+            Console.WriteLine("A continuacion de la informacion del primer vehiculo a añadir: ");
+            Console.WriteLine("Modelo: ");
+            string modelo = Console.ReadLine();
+            Console.WriteLine("Marca: ");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Kilometraje: ");
+            int km = int.Parse(Console.ReadLine());
+            Console.WriteLine("Año: ");
+            int anio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Precio: ");
+            decimal precio = decimal.Parse(Console.ReadLine());
+
+            //procedemos a añadir la informacion del nuevo vehiculo haciendo uso del metodo añadir "añadir vehiculo
+            //definido en la clase casa comercial
+
+            cc.aniadirVehiculo(new Vehiculo(1, marca, modelo, km, precio, anio));
+            //ver los vehiculos registrados
+            Console.WriteLine("Desea mostrar los vehiculos? S/N");
+            string resp3 = Console.ReadLine();
+            if (resp3 == "S" || resp3 == "s")
+            {
+                Console.WriteLine("A continuación se mostraran todos los vehiculos registrado");
+                cc2.mostrarVehiculos();
+
+            }
+            //vaciar todos los vehiculos registrados
+            cc.vaciarVehiculo();
+
+            //eleminar todos los vehiculos
+            Console.WriteLine("Desea eliminar todos los vehiculos? S/N");
+            string resp2 = Console.ReadLine();
+            if (resp2 == "S" || resp2 == "s")
+            {
+                cc.eliminarVehiculo();
+            }
+            else
+            {
+                Console.WriteLine("Cancelado.");
+            }
+
+        }
+    }
+}
