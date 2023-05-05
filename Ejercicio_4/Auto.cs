28c8e45cc6ce9ccdedd6912b099ec65113375ace
Jheysson1 <jheyssonmendoza4@gmail.com>
2023-05-04 21:40:48 -0600

    Ejercicio_4

---
+++ Ejercicio_4/Auto.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejercicio_4
+{
+    internal class Auto
+    {
+        private int idAuto;
+        private string marcaAuto;
+        private string modeloAuto;
+        private int kmAuto;
+        private decimal precioAuto;
+        private int annioAuto;
+        private bool airbagAuto;
+
+        public Auto(int idAuto, string marcaAuto, string modeloAuto, int kmAuto, decimal precioAuto, int annioAuto, bool airbagAuto)
+        {
+            this.idAuto = idAuto;
+            this.marcaAuto = marcaAuto;
+            this.modeloAuto = modeloAuto;
+            this.kmAuto = kmAuto;
+            this.precioAuto = precioAuto;
+            this.annioAuto = annioAuto;
+            this.airbagAuto = airbagAuto;
+        }
+    }
+}
---
+++ Ejercicio_4/Moto.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejercicio_4
+{
+    internal class Moto
+    {
+        private int idMoto;
+        private string marcaMoto;
+        private string modeloMoto;
+        private int kmMoto;
+        private decimal precioMoto;
+        private int annioMoto;
+        private bool sidecarMoto;
+
+        public Moto(int idMoto, string marcaMoto, string modeloMoto, int kmMoto, decimal precioMoto, int annioMoto, bool sidecarMoto)
+        {
+            this.idMoto = idMoto;
+            this.marcaMoto = marcaMoto;
+            this.modeloMoto = modeloMoto;
+            this.kmMoto = kmMoto;
+            this.precioMoto = precioMoto;
+            this.annioMoto = annioMoto;
+            this.sidecarMoto = sidecarMoto;
+        }
+    }
+}
